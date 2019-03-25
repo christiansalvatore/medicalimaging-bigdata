f479d8f55335a23f0cb6623069fea2146759b7aa
Christian Salvatore <christian.salvatore@ibfm.cnr.it>
2019-03-25 11:01:28 +0100

    Update compute__area.m

--- practicalsession__3/compute__area.m
+++ practicalsession__3/compute__area.m
@@ -3,87 +3,85 @@
 % matteo.interlenghi@ibfm.cnr.it
 %-------------------------------------------------------------------------
 
-function [area, N] = compute__area(M, voxel__dimension)
+function [surface, N] = compute__surface(M, voxel__dimension)
 
-    x = size(M,1);
-    y = size(M,2);
-    z = size(M,3);
-    dx = voxel__dimension(1,1)/10;
-    dy = voxel__dimension(1,2)/10;
-    dz = voxel__dimension(1,3)/10;
+    x__size = size(M,1); % Size in terms of # voxels
+    y__size = size(M,2); % Size in terms of # voxels
+    z__size = size(M,3); % Size in terms of # voxels
+    dx = voxel__dimension(1,1)/10; % Convert from mm to cm
+    dy = voxel__dimension(1,2)/10; % Convert from mm to cm
+    dz = voxel__dimension(1,3)/10; % Convert from mm to cm
     
-    %Receive 3D matrix, matrix dimensions and voxel dimensions
-    area = 0;
-    N = zeros(x,y,z);
-    for k=1:z
-        for j=1:y
-            if M(1,j,k)>0
-                area=area+(dz*dy);
-                N(1,j,k)=1;
+    surface = 0;
+    N = zeros(x__size,y__size,z__size);
+    for k = 1:z__size
+        for j = 1:y__size
+            if M(1,j,k) > 0
+                surface = surface+(dz*dy);
+                N(1,j,k) = 1;
             end
-            if M(x,j,k)>0
-                area=area+(dz*dy);
-                N(x,j,k)=1;
+            if M(x__size,j,k) > 0
+                surface = surface+(dz*dy);
+                N(x__size,j,k) = 1;
             end
-
         end
     end
-    for k=1:z
-        for i=1:x
-            if M(i,1,k)>0
-                area=area+(dz*dx);
-                N(i,1,k)=1;
+    
+    for k = 1:z__size
+        for i = 1:x__size
+            if M(i,1,k) > 0
+                surface = surface+(dz*dx);
+                N(i,1,k) = 1;
             end
-            if M(i,y,k)>0
-                area=area+(dz*dx);
-                N(i,y,k)=1;
+            if M(i,y__size,k) > 0
+                surface = surface+(dz*dx);
+                N(i,y__size,k) = 1;
             end
-
         end
     end
-    for j=1:y
-        for i=1:x
-            if M(i,j,1)>0
-                area=area+(dy*dx);
-                N(i,j,1)=1;
+    
+    for j = 1:y__size
+        for i = 1:x__size
+            if M(i,j,1) > 0
+                surface = surface+(dy*dx);
+                N(i,j,1) = 1;
             end
-            if M(i,j,z)>0
-                area=area+(dy*dx);
-                N(i,j,z)=1;
+            if M(i,j,z__size) > 0
+                surface = surface+(dy*dx);
+                N(i,j,z__size) = 1;
             end
-
         end
     end
-    for i=1:x
-        for j=1:y
-            for k=1:z
-                if k>1 && M(i,j,k)>0 && M(i,j,k-1)==0
-                    area=area+(dx*dy);
-                    N(i,j,k)=1;
+    
+    for i = 1:x__size
+        for j = 1:y__size
+            for k = 1:z__size
+                if k > 1 && M(i,j,k) > 0 && M(i,j,k-1) == 0
+                    surface = surface+(dx*dy);
+                    N(i,j,k) = 1;
                 end
-                if k<z && M(i,j,k)>0 && M(i,j,k+1)==0
-                    area=area+(dx*dy);
-                    N(i,j,k)=1;
+                if k < z__size && M(i,j,k) > 0 && M(i,j,k+1) == 0
+                    surface = surface+(dx*dy);
+                    N(i,j,k) = 1;
                 end
-                if i>1 && M(i,j,k)>0 && M(i-1,j,k)==0
-                    area=area+(dz*dy);
-                    N(i,j,k)=1;
+                if i > 1 && M(i,j,k) > 0 && M(i-1,j,k) == 0
+                    surface = surface+(dz*dy);
+                    N(i,j,k) = 1;
                 end
-                if i<x && M(i,j,k)>0 && M(i+1,j,k)==0
-                    area=area+(dz*dy);
-                    N(i,j,k)=1;
+                if i < x__size && M(i,j,k) > 0 && M(i+1,j,k) == 0
+                    surface = surface+(dz*dy);
+                    N(i,j,k) = 1;
                 end
-                if j>1 && M(i,j,k)>0 && M(i,j-1,k)==0
-                    area=area+(dz*dx);
-                    N(i,j,k)=1;
+                if j > 1 && M(i,j,k) > 0 && M(i,j-1,k) == 0
+                    surface = surface+(dz*dx);
+                    N(i,j,k) = 1;
                 end
-                if j<y && M(i,j,k)>0 && M(i,j+1,k)==0
-                    area=area+(dz*dx);
-                    N(i,j,k)=1;
+                if j < y__size && M(i,j,k) > 0 && M(i,j+1,k) == 0
+                    surface = surface+(dz*dx);
+                    N(i,j,k) = 1;
                 end
             end
         end
     end
     
 end
-
